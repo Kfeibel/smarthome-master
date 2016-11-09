f7159dd5293301b048aab0f89e77ba8d2192c99b
Kevin Feibel <feibel@convidera.com>
2016-11-09 12:20:07 +0100

    error test

--- js/interface.js
+++ js/interface.js
@@ -2,11 +2,12 @@ function initObjects(){
     console.log('test2');
     jQuery.get('json/devices.json', function(data) {
         console.log('test3');
+        var jsonString = JSON.stringify(data) ;
         console.log(data);
-        var jsonString = data ;
+        
 
         localStorage.setItem("jsonString", jsonString);
-        console.log(localStorage.getItem("jsonString"));
+        console.log(JSON.parse( localStorage.getItem("jsonString") ));
     //console.log(jsonString);
   });
     console.log('test4');
